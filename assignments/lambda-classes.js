c8395717264c5a406093dd48d852c6bee60dd809
ArthurLeonard <ialeonard@ucdavis.edu>
2019-01-24 15:04:57 -0800

    fixed errors and met MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,4 @@
-## `lambda-classes` - We need a roster of Lambda School personnel. Build it!
+/* `lambda-classes` - We need a roster of Lambda School personnel. Build it!
 
 * We have a school to build here! This project will get you used to thinking about classes in JavaScript and building them from a brand new data set.
 * Lambda personnel can be broken down into three different types of `people`.
@@ -18,13 +18,15 @@ const fred = new Instructor({
   catchPhrase: `Don't forget the homies`
 });
 ```
-
+*/
+/*
 #### Person
 
 * First we need a Person class. This will be our `base-class`
 * Person receives `name` `age` `location` `gender` all as props
 * Person receives `speak` as a method.
 * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
+*/
 
 class Person {
     constructor(attributes) {
@@ -44,7 +46,7 @@ class Person {
 
 
 
-
+/*
 #### Instructor
 
 * Now that we have a Person as our base class, we'll build our Instructor class.
@@ -56,7 +58,7 @@ class Person {
 * Instructor has the following methods:
   * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
   * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
-  
+  */
 
   class Instructor extends Person {
       constructor(atts) {
@@ -67,15 +69,15 @@ class Person {
       }
 
       demo(subject) {
-          console.log(`Today we are learning about {subject}.`);
+          console.log(`Today we are learning about ${subject}.`);
       }
 
       grade(student, subject) {
-          console.log('{student.name} receives a perfect score on {subject}.');
+          console.log(`${student.name} receives a perfect score on ${subject}.`);
       }
   } // end Instructor class
 
-#### Student
+/*#### Student
 
 * Now we need some students!
 * Student uses the same attributes that have been set up by Person
@@ -87,9 +89,9 @@ class Person {
   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
-  
+  */
 
-  class Students extends Person{
+  class Student extends Person{
         constructor(atts) {
             super(atts);
             this.previousBackground = atts.previousBackground;
@@ -102,15 +104,15 @@ class Person {
         }
 
         PRAssignment(subject) {
-            console.log(`${this.name} has submitted a PR for {subject}.`);
+            console.log(`${this.name} has submitted a PR for ${subject}.`);
         }
 
         sprintChallenge(subject) {
-            console.log(`${this.name} has begun sprint challenge on {subject}.`);
+            console.log(`${this.name} has begun sprint challenge on ${subject}.`);
         }
   } // end Student class
 
-#### Project Mananger
+/*#### Project Mananger
 
 * Now that we have instructors and students, we'd be nowhere without our PM's
 * ProjectManagers are extensions of Instructors
@@ -120,7 +122,7 @@ class Person {
 * ProjectManangers have the following Methods:
   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
-  
+  */
 
     class PM extends Instructor {
         constructor(atts) {
@@ -131,25 +133,25 @@ class Person {
         }
 
         standUp(channel) {
-            console.log(`${this.name} announces to {channel}; @channel standup times!`);
+            console.log(`${this.name} announces to ${channel}; @channel standup times!`);
         }
 
         debugsCode(student, subject) {
-            console.log(`${this.name} debugs {student.name}'s code on {subject}`);
+            console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
 
         }
     } // end PM class
 
 
 
-#### Stretch Problem
+/*#### Stretch Problem
 
 * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
 * Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
 * Add a graduate method to a student.
   * This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
   * If the student's grade is above a 70% let them graduate! Otherswise go back to grading their assignments to increase their score.
-
+*/
 
 
 
